730800e2e322a6343d02f5c4618ab45ad1331c9e
piotros <piotros@wp.pl>
2017-08-26 12:55:07 +0200

    Add prettier

--- src/views/DrinkDetails.js
+++ src/views/DrinkDetails.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
-import Drink from "../components/Drink/Drink";
+import React, { Component } from 'react'
+import Drink from '../components/Drink/Drink'
 
 class DrinkDetails extends Component {
-    render() {
-        return (
-            <div className="drink-details">
-                <Drink />
-                <Drink />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="drink-details">
+        <Drink />
+        <Drink />
+      </div>
+    )
+  }
 }
 
-export default DrinkDetails;
+export default DrinkDetails
