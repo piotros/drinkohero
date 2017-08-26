e17a2c8f6237b6e2c6bd27c5a4db083209de14ca
dfuser <patryk.gulas@gmail.com>
2017-08-26 14:52:27 +0200

    add navigation to drink detail

--- src/views/DrinkDetails.js
+++ src/views/DrinkDetails.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import {Paper} from "material-ui";
+import { Paper } from 'material-ui'
+import Drink from '../components/Drink/Drink'
 
 class DrinkDetails extends Component {
   render() {
     return (
-        <div className="drink-details">
-            <Paper>
-            <p>Szczegóły drinku</p>
-            </Paper>
-        </div>
+      <div className="drink-details">
+        <Paper>
+          {/*<Drink />*/}
+        </Paper>
+      </div>
     )
   }
 }
